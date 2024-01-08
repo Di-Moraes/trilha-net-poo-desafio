60428b448c0844f13e80c288f938a4bd9dd2a59d
Edson de Moraes <diimoraessz@gmail.com>
2024-01-08 18:32:11 -0300

    Terminado o desafio

--- Program.cs
+++ Program.cs
@@ -5,9 +5,17 @@ Nokia i234 = new Nokia("8888-8888", "i234", "PDF-099", 32);
 string gta = "GTA San Andreas: Mobile";
 string Netflix = "Netflix: Filmes e Séries";
 
+Console.WriteLine("Iphone:");
 
-Console.WriteLine("Olá, bem-vindo ao simulador virtual de smartphones, por favor digite a marca:\n 1 - Nokia\n 2 - Iphone");
+X999.Ligar();
+X999.ReceberLigacao();
+X999.InstalarAplicativo(gta);
+X999.InstalarAplicativo(Netflix);
 
+Console.WriteLine("Nokia:");
 
-
+i234.Ligar();
+i234.ReceberLigacao();
+i234.InstalarAplicativo(gta);
+i234.InstalarAplicativo(Netflix);
 // TODO: Realizar os testes com as classes Nokia e Iphone
