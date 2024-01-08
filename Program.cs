9ee7a13111359a85159693b40d586c7b15d40279
unknown <diimoraessz@gmail.com>
2024-01-08 18:18:50 -0300

    Erros corrigidos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
+Iphone X999 = new Iphone("9999-9999", "X999", "EDF-999", 64);
+Nokia i234 = new Nokia("8888-8888", "i234", "PDF-099", 32);
+
+string gta = "GTA San Andreas: Mobile";
+string Netflix = "Netflix: Filmes e Séries";
+
+
+Console.WriteLine("Olá, bem-vindo ao simulador virtual de smartphones, por favor digite a marca:\n 1 - Nokia\n 2 - Iphone");
+
+
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
